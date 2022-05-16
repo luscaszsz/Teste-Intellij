a90a745d81399cc959bb534375c8de7f21e09297
Lucas Tomaz <lucastomaz44lt@gmail.com>
2022-05-16 10:00:28 -0300

    teste

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        int a, b;
+        a = 9;
+        b = 10;
+        System.out.println("Soma de a + b = " + (a + b));
+        System.out.println(gato);
+    }
+}
